3483561bfbacb23255578e5d3d37df4da1e3b9d1
Yaeri Lim <yaerilim@gmail.com>
2017-01-23 12:11:39 -0800

    Memory Match v1.0 - Yaeri Lim

--- script.js
+++ script.js
@@ -1,38 +1,55 @@
-/**
- * Created by yaeri on 1/17/2017.
- */
 $(document).ready(initializeGame);
 
 function initializeGame(){
     console.log('initializeGame called');
     $('.card').click(card_clicked);
+    $('.reset_button').click(function (){
+        games_played++;
+        reset_stats();
+        $('.card').show();
+        $('.youWon').hide();
+    })
 }
-
 var first_card_clicked = null;
 var second_card_clicked = null;
-var total_possible_matches = 2;
+var total_possible_matches = 9;
 var match_counter = 0;
+var matches = 0;
+var attempts = 0;
+var accuracy = 0;
+var games_played = 0;
 
 function card_clicked(){
-    console.log('play_card called',this);
-            var back_element = $(this).find('.back');
-            back_element.hide();
-            if(first_card_clicked == null){
-                first_card_clicked = $(this);
+    if($(this).find('.back').css('display') == 'none'){
+        return;
+    }
+    var back_element = $(this).find('.back');
+    back_element.hide();
+    if(first_card_clicked == null){
+        first_card_clicked = $(this);
+    }
+    else if (first_card_clicked!==null){
+        second_card_clicked = $(this);
+        ++attempts;
+        display_stats();
+        console.log('attempts made: ' + attempts);
+        var first_card_front = first_card_clicked.find('.front').find('img').attr('src');
+        var second_card_front = second_card_clicked.find('.front').find('img').attr('src');
+        if(first_card_front == second_card_front){
+            console.log('Cards matched');
+            ++match_counter;
+            ++matches;
+            accuracy = Math.floor(matches / attempts *100);
+            console.log('number of cards matched: ' + matches);
+            console.log('accuracy: ' + accuracy);
+            first_card_clicked = null;
+            second_card_clicked = null;
+            if(match_counter == total_possible_matches){
+                $('.card').hide();
+                $('#game-area').append("<div class='youWon'><img class='youWon_image' src='img/you_won.gif'><br><p>Great Job!</p></div>");
             }
-            else if (first_card_clicked!==null){
-                second_card_clicked = $(this);
-                var first_card_front = first_card_clicked.find('.front').find('img').attr('src');
-                var second_card_front = second_card_clicked.find('.front').find('img').attr('src');
-                if(first_card_front == second_card_front){
-                    console.log('Cards matched');
-                    ++match_counter;
-                    first_card_clicked = null;
-                    second_card_clicked = null;
-                    if(match_counter == total_possible_matches){
-                        $('body').text('You Won')
-                    }
         }else{
+            $('.card').off();
             setTimeout(function(){
                 var first_card_back = $(first_card_clicked).find('.back');
                 var second_card_back = $(second_card_clicked).find('.back');
@@ -40,7 +57,21 @@ function card_clicked(){
                 second_card_back.show();
                 first_card_clicked = null;
                 second_card_clicked = null;
+                $('.card').click(card_clicked);
             }, 2000);
         }
     }
+}
+function display_stats(){
+    $('.games-played .value').text(games_played);
+    $('.attempts .value').text(attempts);
+    $('.accuracy .value').text(accuracy + '%');
+}
+function reset_stats(){
+    accuracy = 0;
+    matches = 0;
+    attempts = 0;
+    match_counter = 0;
+    display_stats();
+    $('.card').find('.back').show();
 }
